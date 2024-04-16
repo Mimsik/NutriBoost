dd52dcb4b9255811d14c3e31d3d89ab209579534
Drinceanu Adel <adel_drinceanu@yahoo.com>
2024-04-16 20:36:28 +0300

    added links to login and register page

--- lib/pages/register_page.dart
+++ lib/pages/register_page.dart
@@ -1,27 +1,31 @@
 import 'package:flutter/material.dart';
 import 'package:nutri_boost/components/my_button.dart';
 import 'package:nutri_boost/components/my_textfield.dart';
-import 'package:nutri_boost/pages/login_page.dart'; // Import LoginPage
+import 'package:nutri_boost/components/square_tile.dart';
+import 'login_page.dart';
 
-class RegisterPage extends StatelessWidget {
-  RegisterPage({Key? key}) : super(key: key);
+class SignUpPage extends StatelessWidget {
+  SignUpPage({Key? key});
 
+  // text editing controllers
   final usernameController = TextEditingController();
   final passwordController = TextEditingController();
-  final nameController = TextEditingController();
-  final confirmPasswordController = TextEditingController();
 
-  void signUserIn() {}
+  // sign user in method
+  void SingUpUser() {}
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.transparent,
+      backgroundColor:
+          Colors.transparent, // Set background color to transparent
       body: Container(
         decoration: BoxDecoration(
           image: DecorationImage(
-            image: AssetImage("lib/images/background_image.jpeg"),
-            fit: BoxFit.cover,
+            image: AssetImage(
+                'lib/images/background_image.jpeg'), // Set your image path here
+            fit: BoxFit
+                .cover, // You can change the BoxFit according to your requirement
           ),
         ),
         child: SafeArea(
@@ -31,55 +35,69 @@ class RegisterPage extends StatelessWidget {
                 mainAxisAlignment: MainAxisAlignment.center,
                 children: [
                   const SizedBox(height: 10),
+                  //Logo
                   Positioned(
                     left: 250,
                     top: 0,
                     child: Image.asset('lib/images/logo.png',
                         width: 200, height: 200),
                   ),
-                  const SizedBox(height: 50),
-                  Text(
-                    'Welcome !',
-                    style: TextStyle(
-                      color: Colors.grey[700],
-                      fontSize: 16,
-                    ),
-                  ),
-                  const SizedBox(height: 25),
+
+                  const SizedBox(height: 10),
+
+                  // username textfield
                   MyTextField(
                     controller: usernameController,
                     hintText: 'Name',
                     obscureText: false,
                   ),
+
                   const SizedBox(height: 10),
+
+                  // password textfield
                   MyTextField(
                     controller: passwordController,
                     hintText: 'Email',
+                    obscureText: true,
+                  ),
+
+                  const SizedBox(height: 10),
+
+                  // username textfield
+                  MyTextField(
+                    controller: usernameController,
+                    hintText: 'Password',
                     obscureText: false,
                   ),
+
                   const SizedBox(height: 10),
+
+                  // password textfield
                   MyTextField(
-                    controller: nameController,
-                    hintText: 'Password',
+                    controller: passwordController,
+                    hintText: 'Confirm password',
                     obscureText: true,
                   ),
-                  const SizedBox(height: 10),
-                  MyTextField(
-                    controller: confirmPasswordController,
-                    hintText: 'Confirm Password',
-                    obscureText: true,
-                  ),
-                  const SizedBox(height: 25),
+
+                  const SizedBox(height: 40),
+
+                  // Log In button
                   MyButton(
-                    onTap: signUserIn,
+                    onTap: SingUpUser, // Specify your sign up function
+                    text: 'Sign Up', // Text for sign up button
                   ),
-                  const SizedBox(height: 50),
+
+                  const SizedBox(height: 10),
+
+                  // not a member? register now
                   Row(
                     mainAxisAlignment: MainAxisAlignment.center,
                     children: [
                       Text(
                         'Already have an account?',
-                        style: TextStyle(color: Colors.grey[700]),
+                        style: TextStyle(
+                          color: Colors.black,
+                        ),
                       ),
                       const SizedBox(width: 4),
                       GestureDetector(
@@ -87,16 +105,16 @@ class RegisterPage extends StatelessWidget {
                           Navigator.push(
                             context,
                             MaterialPageRoute(
-                              builder: (context) => LoginPage(),
-                            ),
+                                builder: (context) => LogInPage()),
                           );
                         },
                         child: Text(
-                          'Sign in here',
+                          'Log In',
                           style: TextStyle(
-                            color: Colors.blue,
+                            color: Color.fromARGB(255, 173, 126, 255),
                             fontWeight: FontWeight.bold,
-                            decoration: TextDecoration.underline,
+                            decoration:
+                                TextDecoration.underline, // Add underline style
                           ),
                         ),
                       ),
